4a518af9168808962e3cf9bf9f28ec5ada078fb9
Prince Patel <105659456+REPATEL6@users.noreply.github.com>
2024-08-22 15:05:35 +0530

    Updated crucial file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyA3Rnv5QD2gq3LWlhNH0WyrLGQYtoGpcCI",
+        apiKey: "xxx",
         authDomain: "codebin-ng-demo.firebaseapp.com",
         databaseURL: "https://codebin-ng-demo-default-rtdb.firebaseio.com/",
         projectId: "codebin-ng-demo",
         storageBucket: "codebin-ng-demo.appspot.com",
-        messagingSenderId: "435615179321",
-        appId: "1:435615179321:web:9eb2d80f0700f95e8623e8"
+        messagingSenderId: "xxx",
+        appId: "xxx"
     }
   };
